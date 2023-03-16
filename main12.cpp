92043f0d28182308e2d8ba5acb36731c2994d6bf
OlegKondr <125397427+OlegKondr@users.noreply.github.com>
2023-03-16 15:38:37 +0300

    Add files via upload

---
+++ main12.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+ main() 
+{
+   int mas[100];
+   int n;
+   
+   printf("Array element\n");
+   scanf("%i",&n);
+   
+   for(int i=0;i<n;i++)
+   {
+   	scanf("%i",&mas[i]);
+   }
+   
+   int min,max;
+   min=max=mas[0];
+   
+   int sum=0;
+   
+   for(int i=0;i<n;i++)
+   {
+   	    if(mas[i]>max)
+   	{
+   	max=mas[i];
+    }
+    	if(mas[i]<min)
+   	{
+   	min=mas[i];
+    }
+    
+    
+	sum+=mas[i];
+   }
+   
+   float sr=(float)sum/(float)n;
+   
+   
+   printf("Sum=%i  Sr=%f  Min=%i  Max=%i",sum,sr,min,max);
+   
+}
